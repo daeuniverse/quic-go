b14d97b97de82fcb8d38da6a5404e094c8bb6f47
wwqgtxx <wwqgtxx@gmail.com>
2023-06-03 14:30:38 +0800

    change module path

--- integrationtests/versionnegotiation/versionnegotiation_suite_test.go
+++ integrationtests/versionnegotiation/versionnegotiation_suite_test.go
@@ -7,10 +7,10 @@ import (
 	"flag"
 	"testing"
 
-	"github.com/quic-go/quic-go/integrationtests/tools"
-	"github.com/quic-go/quic-go/logging"
+	"github.com/metacubex/quic-go/integrationtests/tools"
+	"github.com/metacubex/quic-go/logging"
 
-	"github.com/quic-go/quic-go"
+	"github.com/metacubex/quic-go"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
--- transport.go
+++ transport.go
@@ -9,11 +9,11 @@ import (
 	"sync"
 	"time"
 
-	"github.com/quic-go/quic-go/internal/wire"
+	"github.com/metacubex/quic-go/internal/wire"
 
-	"github.com/quic-go/quic-go/internal/protocol"
-	"github.com/quic-go/quic-go/internal/utils"
-	"github.com/quic-go/quic-go/logging"
+	"github.com/metacubex/quic-go/internal/protocol"
+	"github.com/metacubex/quic-go/internal/utils"
+	"github.com/metacubex/quic-go/logging"
 )
 
 type Transport struct {
@@ -281,7 +281,7 @@ func (t *Transport) listen(conn rawConn) {
 		//nolint:staticcheck // SA1019 ignore this!
 		// TODO: This code is used to ignore wsa errors on Windows.
 		// Since net.Error.Temporary is deprecated as of Go 1.18, we should find a better solution.
-		// See https://github.com/quic-go/quic-go/issues/1737 for details.
+		// See https://github.com/metacubex/quic-go/issues/1737 for details.
 		if nerr, ok := err.(net.Error); ok && nerr.Temporary() {
 			t.mutex.Lock()
 			closed := t.closed
