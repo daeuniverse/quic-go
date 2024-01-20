f80d37a5a13dead483b4d27434b5c48195db29a2
wwqgtxx <wwqgtxx@gmail.com>
2024-01-20 09:01:00 +0800

    Revert "drop support for Go 1.20, build on Go 1.22rc1 on CI (#4195)"
    This reverts commit d795250479b831c1a770703bd8d0d8a748d70d5e.

--- internal/handshake/crypto_setup_test.go
+++ internal/handshake/crypto_setup_test.go
@@ -9,6 +9,8 @@ import (
 	"math/big"
 	"net"
 	"reflect"
+	"runtime"
+	"strings"
 	"time"
 
 	mocktls "github.com/quic-go/quic-go/internal/mocks/tls"
@@ -460,8 +462,10 @@ var _ = Describe("Crypto Setup TLS", func() {
 				Eventually(receivedSessionTicket).Should(BeClosed())
 				Expect(server.ConnectionState().DidResume).To(BeTrue())
 				Expect(client.ConnectionState().DidResume).To(BeTrue())
-				Expect(clientRTTStats.SmoothedRTT()).To(Equal(clientRTT))
-				Expect(serverRTTStats.SmoothedRTT()).To(Equal(serverRTT))
+				if !strings.Contains(runtime.Version(), "go1.20") {
+					Expect(clientRTTStats.SmoothedRTT()).To(Equal(clientRTT))
+					Expect(serverRTTStats.SmoothedRTT()).To(Equal(serverRTT))
+				}
 			})
 
 			It("doesn't use session resumption if the server disabled it", func() {
